118fe4e947733b72bf0ce5a8ebcba8b924b4d15e
soadist <dimag.dn@gmail.com>
2020-11-12 14:21:42 +0200

    add bites to string converter

--- src/net/ddns/dimag/cobhamrunning/utils/Utils.java
+++ src/net/ddns/dimag/cobhamrunning/utils/Utils.java
@@ -11,11 +11,13 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Utils {
     public final static long ONE_MILLISECOND = 1;
@@ -207,6 +209,42 @@ public class Utils {
             e.printStackTrace();
         }
         return foReturn;
+    }
+
+    public static <K, V> K getKey(Map<K, V> map, V value) {
+        return map.keySet()
+                .stream()
+                .filter(key -> value.equals(map.get(key)))
+                .findFirst().get();
+    }
+
+
+    private static final long K = 1024;
+    private static final long M = K * K;
+    private static final long G = M * K;
+    private static final long T = G * K;
+    public static String bytesConverter(final long value){
+        final long[] dividers = new long[] { T, G, M, K, 1 };
+        final String[] units = new String[] { "TB", "GB", "MB", "KB", "B" };
+        if(value < 1)
+            throw new IllegalArgumentException("Invalid file size: " + value);
+        String result = null;
+        for(int i = 0; i < dividers.length; i++){
+            final long divider = dividers[i];
+            if(value >= divider){
+                result = format(value, divider, units[i]);
+                break;
+            }
+        }
+        return result;
+    }
+
+    private static String format(final long value,
+                                 final long divider,
+                                 final String unit){
+        final double result =
+                divider > 1 ? (double) value / (double) divider : (double) value;
+        return new DecimalFormat("#,##0.#").format(result) + " " + unit;
     }
 
 }
